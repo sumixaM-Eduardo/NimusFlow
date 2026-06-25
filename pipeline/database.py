252a023422f70e589d2888d9020b08ed5f481ec9
Maximus <maximus.cruz@penedo.ufal.br>
2026-06-25 10:55:14 -0300

    feat: Data table creation function added

---
+++ pipeline/database.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+pathdb = '/data/sales.db'
+
+def create_table():
+    conn = sqlite3.connect(pathdb)
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS sales (order_id INTEGER, customer_id INTEGER, product_name TEXT, quantity INTEGER, unit_price REAL, sale_date TEXT, payment_method TEXT, city TEXT)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS rejected_sales (order_id TEXT, customer_id TEXT, product_name TEXT, quantity TEXT, unit_price TEXT, sale_date TEXT, payment_method TEXT, city TEXT)')
+    conn.commit()
+    conn.close()
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -6,4 +6,4 @@ def extract():
     with open(RAW_SALES_FILE, "r", encoding='utf-8') as csv_file:
         reader = csv.DictReader(csv_file)
         sales = list(reader)
-        return sales
+    return sales
