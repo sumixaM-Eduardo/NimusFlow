4c74ddd8b4adb351058f4be9c427e428861ca048
Maximus <maximus.cruz@penedo.ufal.br>
2026-06-25 10:24:27 -0300

    feat: Data conversion function added

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def clean_data(sales):
     for sale in sales:
         sale['order_id'] = sale['order_id'].strip()
@@ -9,3 +11,39 @@ def clean_data(sales):
         sale['payment_method'] = sale['payment_method'].strip()
         sale['city'] = sale['city'].strip()
     return sales
+
+def convert_data(sales):
+    valid_sales = []
+    invalid_sales = []
+    for sale in sales:
+        try:
+            sale['order_id'] = int(sale['order_id'])
+        except ValueError:
+            invalid_sales.append(sale)
+            continue
+        try:
+            sale['customer_id'] = int(sale['customer_id'])
+        except ValueError:
+            invalid_sales.append(sale)
+            continue
+        try:
+            sale['quantity'] = int(sale['quantity'])
+        except ValueError:
+            invalid_sales.append(sale)
+            continue
+        try:
+            sale['unit_price'] = float(sale['unit_price'])
+        except ValueError:
+            invalid_sales.append(sale)
+            continue
+        try:
+            sale['sale_date'] = datetime.strptime(sale['sale_date'], '%Y-%m-%d')
+        except ValueError:
+            invalid_sales.append(sale)
+            continue
+        valid_sales.append(sale)
+    return valid_sales, invalid_sales
+
+
+
+
